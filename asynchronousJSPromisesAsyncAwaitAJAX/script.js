1d5083a1e6b1cac395bc47623d813e3de666a6ef
Dal S Garbuja Pun <dal@garbuja.pun>
2023-01-18 18:03:19 +1000

    Consuming Promises

--- asynchronousJSPromisesAsyncAwaitAJAX/script.js
+++ asynchronousJSPromisesAsyncAwaitAJAX/script.js
@@ -4,6 +4,38 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+const renderCountry = function (country) {
+  // console.log(country);
+  const html = `
+        <article class="country ">
+          <img class="country__img" src="${country.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${country.name}</h3>
+            <h4 class="country__region">${country.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +country.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${
+              country.languages[0].name
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              country.currencies[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
 
-const request = fetch("https://restcountries.com/v2/name/nepal");
-console.log(request);
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      renderCountry(data[0]);
+    });
+};
+
+getCountryData("nepal");
+getCountryData("australia");
