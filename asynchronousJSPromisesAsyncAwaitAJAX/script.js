878d9e5110d8692f2bad79dc0604e32b6e26b8d2
Dal S Garbuja Pun <dal@garbuja.pun>
2023-01-20 13:31:48 +1000

    Challenge #1

--- asynchronousJSPromisesAsyncAwaitAJAX/script.js
+++ asynchronousJSPromisesAsyncAwaitAJAX/script.js
@@ -16,9 +16,11 @@ const renderCountry = function (country, className = "") {
           <div class="country__data">
             <h3 class="country__name">${country.name}</h3>
             <h4 class="country__region">${country.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +country.population / 1000000
-            ).toFixed(1)} people</p>
+            <p class="country__row"><span>👫</span>${
+              +country.population > 1000000
+                ? (+country.population / 1000000).toFixed(1)
+                : +country.population
+            } people</p>
             <p class="country__row"><span>🗣️</span>${
               country.languages[0].name
             }</p>
@@ -32,64 +34,35 @@ const renderCountry = function (country, className = "") {
   // countriesContainer.style.opacity = 1;
 };
 
-// const getCountryData = function (country) {
-//   fetch(`https://restcountries.com/v2/name/${country}`)
-//     .then((response) => {
-//       console.log(response);
-//       if (!response.ok)
-//         throw new Error(`Country '${country}' not found (${response.status})`);
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       renderCountry(data[0]);
+// Challenge 1
+const whereAmI = function (lat, lon) {
+  fetch(
+    `https://geocode.xyz/${lat},${lon}?geoit=json&auth=119352132948252e15838634x3713`
+  )
+    .then((response) => {
+      if (!response.ok) throw new Error("Only 3 requests per second please!");
+      return response.json();
+    })
+    .then((data) => {
+      console.log(`You are in ${data.city}, ${data.country}`);
 
-//       if (!data[0].borders) throw new Error(`Neighbour country not found`);
-//       const neighbour = data[0].borders[0];
-//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-//     })
-//     .then((response) => response.json())
-//     .then((data) => renderCountry(data, "neighbour"))
-//     .catch((err) => {
-//       console.log(`${err} 💥💥💥`);
-//       renderError(`Something went wrong ${err.message}. Try again!`);
-//     })
-//     .finally(() => (countriesContainer.style.opacity = 1));
-// };
-
-// refactored version
-const getJSON = function (url, errMsg = "Ooops, something went wrong! ") {
-  return fetch(url).then((response) => {
-    if (!response.ok) throw new Error(errMsg);
-    return response.json();
-  });
-};
-
-const getCountryData = function (country) {
-  getJSON(`https://restcountries.com/v2/name/${country}`, "Country not found")
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then((response) => {
+      if (!response.ok) throw new Error("Country not found");
+      return response.json();
+    })
     .then((data) => {
       renderCountry(data[0]);
-
-      if (!data[0].borders) throw new Error(`Neighbour country not found`);
-      const neighbour = data[0].borders[0];
-      return getJSON(
-        `https://restcountries.com/v2/alpha/${neighbour}`,
-        "Neighbour country not found"
-      );
     })
-    .then((data) => renderCountry(data, "neighbour"))
     .catch((err) => {
-      console.log(`${err} 💥💥💥`);
-      renderError(`Something went wrong ${err.message}. Try again!`);
+      console.error(`Something went wrong! ${err.message}`);
+      renderError(err);
     })
     .finally(() => (countriesContainer.style.opacity = 1));
 };
 
-btn.addEventListener("click", function () {
-  // test data for no country found
-  // getCountryData("123");
-  // test data for no neighour found
-  getCountryData("australia");
-  // this should work
-  // getCountryData("nepal");
-});
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+whereAmI(-36.38922, 148.65293);
+whereAmI(27.71558, 85.32528);
