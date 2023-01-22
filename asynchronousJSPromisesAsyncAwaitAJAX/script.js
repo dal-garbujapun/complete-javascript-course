4d27d2de7b932a973e5f4070b2552b45c287bd7c
Dal S Garbuja Pun <dal@garbuja.pun>
2023-01-22 10:11:40 +1000

    Promisifying the geolocation api

--- asynchronousJSPromisesAsyncAwaitAJAX/script.js
+++ asynchronousJSPromisesAsyncAwaitAJAX/script.js
@@ -5,70 +5,79 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
-// building promise
-// const lotteryPromise = new Promise(function (resolve, reject) {
-//   if (Math.random() >= 0.5) {
-//     resolve("YOU WIN! 😁");
-//   } else {
-//     reject("YOU LOST! 😭");
-//   }
-// });
-
-// // consuming promise
-// lotteryPromise
-//   .then((res) => console.log(res))
-//   .catch((res) => console.error(res));
-
-///////////////////////////////////////////////////////////////////////////////
-// simulate the time that is passed betn buying lottery ticket and getting the
-// result to encapsulate asynchoronous behaviour into a promise
-// building promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log("Lottery draw is happening!!!");
-  setTimeout(() => {
-    if (Math.random() >= 0.5) {
-      resolve("YOU WIN!!! 😁");
-    } else {
-      reject(new Error("YOU LOST! 😭"));
-    }
-  }, 2000);
-});
-
-// consuming promise
-lotteryPromise
-  .then((res) => console.log(res))
-  .catch((res) => console.error(res));
-
-///////////////////////////////////////////////////////////////////////////////
-// Promisifying the setTimeout
-// (callback based asynchronous to promised based asynchronous)
-//
-// just like fetch is a function that returns a promise, here we're creating
-// a function that returns a promise
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000); // we don't need any value
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(position),
+      (error) => reject(error)
+    );
+    // navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-// consuming
-wait(1)
-  .then(() => {
-    console.log("I waited for 1 seconds");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("I waited for 2 seconds");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("I waited for 3 seconds");
-  });
+getPosition().then((position) => console.log(position));
 
 ////////////////////////////////////////////////////////////////////////////////
-// Fullfilling or rejecting promise immediately
+// Challenge 1 refactored to not have to pass the lat and lng anymore
+const renderError = function (errorMsg) {
+  countriesContainer.insertAdjacentText("beforeend", errorMsg);
+  // countriesContainer.style.opacity = 1;
+};
 
-Promise.resolve("Insta Fullfilled!").then((res) => console.log(res));
-Promise.reject(new Error("Insta Rejection!")).catch((res) =>
-  console.error(res)
-);
+const renderCountry = function (country, className = "") {
+  const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${country.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${country.name}</h3>
+            <h4 class="country__region">${country.region}</h4>
+            <p class="country__row"><span>👫</span>${
+              +country.population > 1000000
+                ? (+country.population / 1000000).toFixed(1)
+                : +country.population
+            } people</p>
+            <p class="country__row"><span>🗣️</span>${
+              country.languages[0].name
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              country.currencies[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const whereAmI = function () {
+  getPosition()
+    .then((position) => {
+      const { latitude: lat, longitude: lon } = position.coords;
+      return fetch(
+        `https://geocode.xyz/${lat},${lon}?geoit=json&auth=119352132948252e15838634x3713`
+      );
+    })
+    .then((response) => {
+      if (!response.ok) throw new Error("Only 3 requests per second please!");
+      return response.json();
+    })
+    .then((data) => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then((response) => {
+      if (!response.ok) throw new Error("Country not found");
+      return response.json();
+    })
+    .then((data) => {
+      renderCountry(data[0]);
+    })
+    .catch((err) => {
+      console.error(`Something went wrong! ${err.message}`);
+      renderError(err);
+    })
+    .finally(() => (countriesContainer.style.opacity = 1));
+};
+
+whereAmI();
