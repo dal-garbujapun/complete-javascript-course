9b6682b4b37374f11959cec0a4a04250f2143b09
Dal S Garbuja Pun <dal@garbuja.pun>
2023-01-22 19:25:29 +1000

    Challenge #2

--- asynchronousJSPromisesAsyncAwaitAJAX/script.js
+++ asynchronousJSPromisesAsyncAwaitAJAX/script.js
@@ -1,83 +1,45 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
-
-///////////////////////////////////////
-
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => resolve(position),
-      (error) => reject(error)
-    );
-    // navigator.geolocation.getCurrentPosition(resolve, reject);
+const wait = function (sec) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, sec * 1000);
   });
 };
 
-getPosition().then((position) => console.log(position));
+const imageContainer = document.querySelector(".images");
+const createImage = (imgPath) => {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
 
-////////////////////////////////////////////////////////////////////////////////
-// Challenge 1 refactored to not have to pass the lat and lng anymore
-const renderError = function (errorMsg) {
-  countriesContainer.insertAdjacentText("beforeend", errorMsg);
-  // countriesContainer.style.opacity = 1;
+    img.addEventListener("load", function () {
+      // img.classList.add("image");
+      imageContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", () => reject(new Error("Image not found")));
+  });
 };
 
-const renderCountry = function (country, className = "") {
-  const html = `
-        <article class="country ${className}">
-          <img class="country__img" src="${country.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${country.name}</h3>
-            <h4 class="country__region">${country.region}</h4>
-            <p class="country__row"><span>👫</span>${
-              +country.population > 1000000
-                ? (+country.population / 1000000).toFixed(1)
-                : +country.population
-            } people</p>
-            <p class="country__row"><span>🗣️</span>${
-              country.languages[0].name
-            }</p>
-            <p class="country__row"><span>💰</span>${
-              country.currencies[0].name
-            }</p>
-          </div>
-        </article>
-  `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  // countriesContainer.style.opacity = 1;
-};
+let currentImg;
+createImage("img/img-1.jpg")
+  .then((response) => {
+    currentImg = response;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-2.jpg");
+  })
+  .then((response) => {
+    currentImg = response;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+  })
 
-const whereAmI = function () {
-  getPosition()
-    .then((position) => {
-      const { latitude: lat, longitude: lon } = position.coords;
-      return fetch(
-        `https://geocode.xyz/${lat},${lon}?geoit=json&auth=119352132948252e15838634x3713`
-      );
-    })
-    .then((response) => {
-      if (!response.ok) throw new Error("Only 3 requests per second please!");
-      return response.json();
-    })
-    .then((data) => {
-      console.log(`You are in ${data.city}, ${data.country}`);
-
-      return fetch(`https://restcountries.com/v2/name/${data.country}`);
-    })
-    .then((response) => {
-      if (!response.ok) throw new Error("Country not found");
-      return response.json();
-    })
-    .then((data) => {
-      renderCountry(data[0]);
-    })
-    .catch((err) => {
-      console.error(`Something went wrong! ${err.message}`);
-      renderError(err);
-    })
-    .finally(() => (countriesContainer.style.opacity = 1));
-};
-
-whereAmI();
+  .catch((err) => {
+    console.error(`${err}`);
+  });
