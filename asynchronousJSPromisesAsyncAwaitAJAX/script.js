63cbb7c1ea04db4e3693246f13d3b88fc8ae4ca4
Dal S Garbuja Pun <dal@garbuja.pun>
2023-01-22 20:21:31 +1000

    Consuming promises with async await

--- asynchronousJSPromisesAsyncAwaitAJAX/script.js
+++ asynchronousJSPromisesAsyncAwaitAJAX/script.js
@@ -1,45 +1,57 @@
 "use strict";
 
-const wait = function (sec) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, sec * 1000);
-  });
-};
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
-const imageContainer = document.querySelector(".images");
-const createImage = (imgPath) => {
+const renderCountry = function (country, className = "") {
+  const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${country.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${country.name}</h3>
+            <h4 class="country__region">${country.region}</h4>
+            <p class="country__row"><span>👫</span>${
+              +country.population > 1000000
+                ? (+country.population / 1000000).toFixed(1)
+                : +country.population
+            } people</p>
+            <p class="country__row"><span>🗣️</span>${
+              country.languages[0].name
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              country.currencies[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+const getPosition = function () {
   return new Promise(function (resolve, reject) {
-    const img = document.createElement("img");
-    img.src = imgPath;
-
-    img.addEventListener("load", function () {
-      // img.classList.add("image");
-      imageContainer.append(img);
-      resolve(img);
-    });
-
-    img.addEventListener("error", () => reject(new Error("Image not found")));
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(position),
+      (error) => reject(error)
+    );
   });
 };
+const whereAmI = async function (country) {
+  //geolocation
+  const geolocationPosition = await getPosition();
+  const { latitude: lat, longitude: lng } = geolocationPosition.coords;
 
-let currentImg;
-createImage("img/img-1.jpg")
-  .then((response) => {
-    currentImg = response;
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = "none";
-    return createImage("img/img-2.jpg");
-  })
-  .then((response) => {
-    currentImg = response;
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = "none";
-  })
+  // reverse geocode
+  const response = await fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=119352132948252e15838634x3713`
+  );
+  const geocodeData = await response.json();
 
-  .catch((err) => {
-    console.error(`${err}`);
-  });
+  // country data
+  const res = await fetch(
+    `https://restcountries.com/v2/name/${geocodeData.country}`
+  );
+  const data = await res.json();
+  renderCountry(data[0]);
+};
+
+whereAmI();
